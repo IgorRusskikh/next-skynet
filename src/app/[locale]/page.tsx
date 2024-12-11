bdb3f4039bf70de78bfa99c44497a0755658f24c
IgorRusskikh <russkikh200@mail.ru>
2024-12-11 17:55:55 +0700

    feat: remove app in [locale] folder to get locale

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,20 @@
+import About from "@/components/layout/About";
+import FirstScreen from "@/components/layout/FirstScreen";
+import HowToUse from "@/components/layout/HowToUse";
+import Offices from "@/components/layout/Offices";
+import Services from "@/components/layout/Services";
+import Verification from "@/components/layout/Verification";
+
+export default function Home() {
+  return (
+    <main>
+      <FirstScreen />
+      <About />
+      <Services />
+      <HowToUse />
+      <Verification />
+      <Offices />
+      {/* <Blog /> */}
+    </main>
+  );
+}
